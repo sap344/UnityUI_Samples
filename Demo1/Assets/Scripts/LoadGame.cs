9425e7e5a0789ed0453c5cdc4cc2bc980d1d0f62
Shreya Patel <patelshreya94@gmail.com>
2016-10-07 23:05:48 -0400

    Demo1 - highlevel, ready

---
+++ Demo1/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour {
+
+	public void Load(int sceneIndex){
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
---
+++ Demo1/Assets/Scripts/UpdateText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateText : MonoBehaviour {
+
+	private Text myText;
+	float elapsedTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsedTime += Time.deltaTime;
+		myText.text = "Time: " + Mathf.Round(elapsedTime);
+	}
+
+	public void ResetTimer() {
+		elapsedTime = 0f;
+	}
+}
