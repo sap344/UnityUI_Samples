f5d6c5518dbf20761dac4f9360dc1ce02427fe66
Shreya Patel <patelshreya94@gmail.com>
2016-10-15 01:59:33 -0400

    Menu Structure and basic buttons added to a new project

---
+++ Highwater-SD/Assets/Scripts/GUI/MainMenuGUI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuGUI : MonoBehaviour {
+
+	private GameObject settingsPanel;
+	private GameObject endCreditsPanel;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start ()
+	{
+		settingsPanel = GameObject.Find("SettingsPanel");
+		endCreditsPanel = GameObject.Find("EndCreditsPanel");
+		settingsPanel.SetActive (false);
+		endCreditsPanel.SetActive (false);
+	}
+
+	/// <summary>
+	/// Raises the settings button click event.
+	/// </summary>
+	public void OnSettingsClick()
+	{
+		settingsPanel.SetActive (true);
+		endCreditsPanel.SetActive (false);
+	}
+
+	/// <summary>
+	/// Raises the exit button click event.
+	/// </summary>
+	public void OnExitClick()
+	{
+		endCreditsPanel.SetActive (true);
+		settingsPanel.SetActive (false);
+	}
+
+	/// <summary>
+	/// Raises the start button click event.
+	/// </summary>
+	public void OnStartClick()
+	{
+		SceneManager.LoadScene ("Game");
+	}
+
+	/// <summary>
+	/// Raises the back to main button click event.
+	/// </summary>
+	public void OnBackToMainClick()
+	{
+		endCreditsPanel.SetActive (false);
+		settingsPanel.SetActive (false);
+	}
+}
